752bd0506172a95bc57bbea2c0efda0ffd7da059
ImaLi06 <imanollimache@gmail.com>
2026-05-14 00:30:17 -0500

    feat: add routes

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { OrganizationsList } from './space-management/presentation/views/organizations-list/organizations-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: 'organizations', component: OrganizationsList },
+  { path: '', redirectTo: 'organizations', pathMatch: 'full' },
+];
--- src/app/shared/presentation/components/layout/layout.ts
+++ src/app/shared/presentation/components/layout/layout.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { HeaderBar } from '../header-bar/header-bar';
 import { OrganizationsList } from '../../../../space-management/presentation/views/organizations-list/organizations-list';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-layout',
-  imports: [HeaderBar, OrganizationsList],
+  imports: [RouterOutlet],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
