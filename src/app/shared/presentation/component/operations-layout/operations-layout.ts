639f9aba2538fde19b712598edf2b8bab6e2e3b1
ImaLi06 <imanollimache@gmail.com>
2026-05-25 16:32:18 -0500

    feature: add sidebar items

--- src/app/shared/presentation/component/operations-layout/operations-layout.ts
+++ src/app/shared/presentation/component/operations-layout/operations-layout.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import { MatSidenavContainer, MatSidenavContent} from '@angular/material/sidenav';
+import {MatIcon} from '@angular/material/icon';
+import {MatButton} from '@angular/material/button';
 
 @Component({
   selector: 'app-operations-layout',
-  imports: [],
+  imports: [
+    MatSidenavContainer,
+    MatSidenavContent,
+    MatIcon,
+    MatButton
+  ],
   templateUrl: './operations-layout.html',
   styleUrl: './operations-layout.css',
 })
-export class OperationsLayout {}
+export class OperationsLayout {
+  protected items = [
+    { label: "Dashboard", icon: "dashboard", route: ""},
+    { label: "People", icon: "accessibility_new", route: ""},
+    { label: "Devices", icon: "door_back", route: ""},
+    { label: "Rules", icon: "gavel", route: ""},
+    { label: "Audit", icon: "search_activity", route: ""},
+    { label: "Guests", icon: "shield_person", route: ""}
+  ];
+
+  navigateTo(route: string, index: number): void{
+  }
+}
