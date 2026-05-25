dfd2fda81195ed757b9f71ab8bf252669b8bc68f
ImaLi06 <imanollimache@gmail.com>
2026-05-25 18:15:43 -0500

    feat(topbar): redirect to home when clicking the app title

--- src/app/shared/presentation/component/layout/layout.ts
+++ src/app/shared/presentation/component/layout/layout.ts
@@ -1,9 +1,9 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {MatToolbar, MatToolbarRow} from '@angular/material/toolbar';
 import {MatButton, MatIconButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
 import {LanguageSwitcher} from '../language-switcher/language-switcher';
-import {RouterOutlet} from '@angular/router';
+import {Router, RouterOutlet} from '@angular/router';
 import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
 import {MatDivider} from '@angular/material/list';
 
@@ -17,11 +17,16 @@ import {MatDivider} from '@angular/material/list';
     RouterOutlet,
     MatMenuTrigger,
     MatMenu,
-    MatButton,
     MatDivider,
     MatMenuItem,
   ],
   templateUrl: './layout.html',
   styleUrl: './layout.css',
 })
-export class Layout {}
+export class Layout {
+  protected router = inject(Router);
+
+  navigateTo(rute: string){
+    this.router.navigate([rute]).then();
+  }
+}
