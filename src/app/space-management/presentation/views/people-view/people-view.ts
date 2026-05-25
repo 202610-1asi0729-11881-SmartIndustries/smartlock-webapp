47744ecb7c453b00d1d53832aa0bebf490df47af
ImaLi06 <imanollimache@gmail.com>
2026-05-25 17:16:38 -0500

    feat: create people-view component

---
+++ src/app/space-management/presentation/views/people-view/people-view.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-people-view',
+  imports: [],
+  templateUrl: './people-view.html',
+  styleUrl: './people-view.css',
+})
+export class PeopleView {}
