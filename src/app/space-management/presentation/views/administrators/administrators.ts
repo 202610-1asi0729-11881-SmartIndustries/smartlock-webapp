0f0a5aa5ae31753bece4d9c6cb4373b99419092c
ImaLi06 <imanollimache@gmail.com>
2026-05-25 17:22:00 -0500

    feat: create administrators view

---
+++ src/app/space-management/presentation/views/administrators/administrators.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-administrators',
+  imports: [],
+  templateUrl: './administrators.html',
+  styleUrl: './administrators.css',
+})
+export class Administrators {}
