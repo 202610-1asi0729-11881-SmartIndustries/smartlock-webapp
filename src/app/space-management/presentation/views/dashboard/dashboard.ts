c95973b16de0395e941230827771c5973979f6be
ImaLi06 <imanollimache@gmail.com>
2026-05-26 18:43:40 -0500

    feat(dashboard): add recent alerts table

--- src/app/space-management/presentation/views/dashboard/dashboard.ts
+++ src/app/space-management/presentation/views/dashboard/dashboard.ts
@@ -39,6 +39,18 @@ export class Dashboard {
     {person: "Imanol Limache", deviceId: 2, result: true},
     {person: "Imanol Limache", deviceId: 4, result: true},
   ]
-
   protected recentAccessAttemptsColumns: string[] = ["person", "deviceId", "result"];
+
+  protected recentAlerts: {person: string, type: string, date: string}[] = [
+    {person: "Juan Perez", type: "Repeated", date: "18-05-2026 / 16:14"},
+    {person: "Juan Perez", type: "Repeated", date: "18-05-2026 / 16:14"},
+    {person: "Juan Perez", type: "Repeated", date: "18-05-2026 / 16:14"},
+  ]
+  protected recentAlertsColumns: string[] = ["person", "type", "date"];
+
+  protected offlineDevices: {id: number, name: string, site: string}[] = [
+    {id: 1, name: "Main entrance access", site: "North campus"},
+    {id: 2, name: "Main entrance access", site: "North campus"},
+    {id: 3, name: "Main entrance access", site: "North campus"},
+  ]
 }
