9aeccab4f3c1fe241bfe8e1cf9652f495602a3bf
ImaLi06 <imanollimache@gmail.com>
2026-05-26 16:01:06 -0500

    feat(home): add menu for organizations cards

--- src/app/space-management/presentation/components/organization-card/organization-card.ts
+++ src/app/space-management/presentation/components/organization-card/organization-card.ts
@@ -2,6 +2,7 @@ import {Component, input} from '@angular/core';
 import {MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle} from '@angular/material/card';
 import {MatIcon} from '@angular/material/icon';
 import {MatIconButton} from '@angular/material/button';
+import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
 
 @Component({
   selector: 'app-organization-card',
@@ -12,7 +13,10 @@ import {MatIconButton} from '@angular/material/button';
     MatCardSubtitle,
     MatCardContent,
     MatIcon,
-    MatIconButton
+    MatIconButton,
+    MatMenu,
+    MatMenuTrigger,
+    MatMenuItem
   ],
   templateUrl: './organization-card.html',
   styleUrl: './organization-card.css',
