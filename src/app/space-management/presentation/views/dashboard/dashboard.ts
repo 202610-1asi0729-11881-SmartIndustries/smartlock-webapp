8041351891967a72bc74a5653c83b49eecbd19a8
ImaLi06 <imanollimache@gmail.com>
2026-05-26 18:24:18 -0500

    feat(dashboard): add recent access attemps table to dashboard

--- src/app/space-management/presentation/views/dashboard/dashboard.ts
+++ src/app/space-management/presentation/views/dashboard/dashboard.ts
@@ -1,13 +1,44 @@
 import { Component } from '@angular/core';
-import {MatCard, MatCardContent} from '@angular/material/card';
+import {MatCard, MatCardContent, MatCardHeader, MatCardTitle} from '@angular/material/card';
+import {
+  MatCell,
+  MatCellDef,
+  MatColumnDef,
+  MatHeaderCell,
+  MatHeaderCellDef,
+  MatHeaderRow, MatHeaderRowDef, MatRow, MatRowDef,
+  MatTable
+} from '@angular/material/table';
 
 @Component({
   selector: 'app-dashboard',
   imports: [
     MatCardContent,
-    MatCard
+    MatCard,
+    MatCardHeader,
+    MatCardTitle,
+    MatTable,
+    MatColumnDef,
+    MatHeaderCell,
+    MatCell,
+    MatCellDef,
+    MatHeaderCellDef,
+    MatHeaderRow,
+    MatRow,
+    MatHeaderRowDef,
+    MatRowDef
   ],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
-export class Dashboard {}
+export class Dashboard {
+  protected recentAccessAttempts : {person: string, deviceId: number, result: boolean }[] = [
+    {person: "Juan Perez", deviceId: 1, result: true},
+    {person: "Imanol Limache", deviceId: 2, result: true},
+    {person: "Juan Perez", deviceId: 3, result: true},
+    {person: "Imanol Limache", deviceId: 2, result: true},
+    {person: "Imanol Limache", deviceId: 4, result: true},
+  ]
+
+  protected recentAccessAttemptsColumns: string[] = ["person", "deviceId", "result"];
+}
