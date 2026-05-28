77601d9ad3db2ba1e97bdb0b83d250f0b4927365
ImaLi06 <imanollimache@gmail.com>
2026-05-28 11:34:18 -0500

    feat(home): add delete organization form

--- src/app/space-management/presentation/components/organization-card/organization-card.ts
+++ src/app/space-management/presentation/components/organization-card/organization-card.ts
@@ -1,9 +1,11 @@
-import {Component, input} from '@angular/core';
+import {Component, inject, input} from '@angular/core';
 import {MatCard, MatCardContent, MatCardHeader, MatCardSubtitle, MatCardTitle} from '@angular/material/card';
 import {MatIcon} from '@angular/material/icon';
 import {MatIconButton} from '@angular/material/button';
+import {MatDialog} from '@angular/material/dialog';
 import {MatMenu, MatMenuItem, MatMenuTrigger} from '@angular/material/menu';
 import {TranslatePipe} from '@ngx-translate/core';
+import {DeleteOrganizationDialog} from '../delete-organization-form/delete-organization-form';
 
 @Component({
   selector: 'app-organization-card',
@@ -24,5 +26,12 @@ import {TranslatePipe} from '@ngx-translate/core';
   styleUrl: './organization-card.css',
 })
 export class OrganizationCard {
+  protected readonly dialog = inject(MatDialog);
   organization = input.required<{name: string, owner: string, description: string}>();
+
+  protected openDeleteDialog() {
+    this.dialog.open(DeleteOrganizationDialog, {
+      data: this.organization().name,
+    });
+  }
 }
