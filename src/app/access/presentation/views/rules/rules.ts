63c306ae395686e70ac89046015d2d379260ae3c
ImaLi06 <imanollimache@gmail.com>
2026-05-28 20:32:32 -0500

    feat(rules): add filter bar and rules table

--- src/app/access/presentation/views/rules/rules.ts
+++ src/app/access/presentation/views/rules/rules.ts
@@ -1,9 +1,29 @@
 import { Component } from '@angular/core';
+import {MatFormField, MatLabel} from '@angular/material/input';
+import {MatOption} from '@angular/material/core';
+import {MatSelect} from '@angular/material/select';
+import {MatIcon} from '@angular/material/icon';
+import {MatIconButton} from '@angular/material/button';
+import {RulesTable} from '../../components/rules-table/rules-table';
+import {SearchBar} from '../../../../shared/presentation/component/search-bar/search-bar';
 
 @Component({
   selector: 'app-rules',
-  imports: [],
+  imports: [
+    SearchBar,
+    MatFormField,
+    MatLabel,
+    MatOption,
+    MatSelect,
+    MatIcon,
+    MatIconButton,
+    RulesTable,
+    SearchBar
+  ],
   templateUrl: './rules.html',
   styleUrl: './rules.css',
 })
-export class Rules {}
+export class Rules {
+  protected siteOptions: string[] = ['all','North campus', 'East campus', 'West campus'];
+  protected statusOptions: string[] = ['Active', 'Disabled'];
+}
