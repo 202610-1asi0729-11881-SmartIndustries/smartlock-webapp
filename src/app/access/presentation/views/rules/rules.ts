5ac0c3044e9ab10083d9b8e1043a5da9cfd7b809
ImaLi06 <imanollimache@gmail.com>
2026-05-29 09:42:22 -0500

    feat(rules): add create-rule-form

--- src/app/access/presentation/views/rules/rules.ts
+++ src/app/access/presentation/views/rules/rules.ts
@@ -1,11 +1,13 @@
-import { Component } from '@angular/core';
+import {Component, inject} from '@angular/core';
 import {MatFormField, MatLabel} from '@angular/material/input';
 import {MatOption} from '@angular/material/core';
 import {MatSelect} from '@angular/material/select';
 import {MatIcon} from '@angular/material/icon';
 import {MatIconButton} from '@angular/material/button';
+import {MatDialog} from '@angular/material/dialog';
 import {RulesTable} from '../../components/rules-table/rules-table';
 import {SearchBar} from '../../../../shared/presentation/component/search-bar/search-bar';
+import {CreateRuleForm} from '../../components/create-rule-form/create-rule-form';
 
 @Component({
   selector: 'app-rules',
@@ -18,12 +20,18 @@ import {SearchBar} from '../../../../shared/presentation/component/search-bar/se
     MatIcon,
     MatIconButton,
     RulesTable,
-    SearchBar
+    SearchBar,
   ],
   templateUrl: './rules.html',
   styleUrl: './rules.css',
 })
 export class Rules {
+  protected readonly dialog = inject(MatDialog);
+
   protected siteOptions: string[] = ['all','North campus', 'East campus', 'West campus'];
   protected statusOptions: string[] = ['Active', 'Disabled'];
+
+  protected openCreateRuleForm() {
+    this.dialog.open(CreateRuleForm);
+  }
 }
