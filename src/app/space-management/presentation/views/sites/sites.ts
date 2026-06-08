dd91f3a2c5e5e6115c42a5ef706fe84a29ce3fb1
ImaLi06 <imanollimache@gmail.com>
2026-06-08 00:16:48 -0500

    feat(sites): add filter bar

--- src/app/space-management/presentation/views/sites/sites.ts
+++ src/app/space-management/presentation/views/sites/sites.ts
@@ -1,8 +1,13 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
+import {SearchBar} from '../../../../shared/presentation/component/search-bar/search-bar';
+import {TranslatePipe} from '@ngx-translate/core';
 
 @Component({
   selector: 'app-sites',
-  imports: [],
+  imports: [
+    SearchBar,
+    TranslatePipe
+  ],
   templateUrl: './sites.html',
   styleUrl: './sites.css',
 })
