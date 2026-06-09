e4e54d0494ca9e828be24643e99165aee92aaaef
ImaLi06 <imanollimache@gmail.com>
2026-06-09 17:40:43 -0500

    feat(organization): create organizations response

---
+++ src/app/space-management/infrastructure/organizations-response.ts
@@ -0,0 +1,11 @@
+import {BaseResource, BaseResponse} from '../../shared/infrastructure/base-response';
+
+export interface OrganizationsResource extends BaseResource {
+  id: number;
+  owner: string;
+  description: string;
+}
+
+export interface OrganizationsResponse extends BaseResponse {
+  organizations: OrganizationsResource[];
+}
