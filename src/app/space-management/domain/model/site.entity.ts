ca448c29262adfd21e9da87b8d42a04f90f89d9c
ImaLi06 <imanollimache@gmail.com>
2026-06-17 01:25:27 -0500

    feat(sites): add organizationId and description to Site entity

--- src/app/space-management/domain/model/site.entity.ts
+++ src/app/space-management/domain/model/site.entity.ts
@@ -1,17 +1,29 @@
 export class Site {
   private _id: number;
+  private _organizationId: number;
   private _name: string;
+  private _description: string;
 
-  constructor(id: number, name: string) {
+  constructor(id: number, organizationId: number, name: string, description: string) {
     this._id = id;
+    this._organizationId = organizationId;
     this._name = name;
+    this._description = description;
   }
 
   get id(): number {
     return this._id;
   }
 
+  get organizationId(): number {
+    return this._organizationId;
+  }
+
   get name(): string {
     return this._name;
+  }
+
+  get description(): string {
+    return this._description;
   }
 }
